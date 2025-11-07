2ab5c2fb2e5591277db5b89678349bcf38103ced
ckoohin <chuhien.nhat@gmail.com>
2025-11-07 15:50:47 +0700

    lab3

--- lab3/3.js
+++ lab3/3.js
@@ -1,12 +1,15 @@
 //ex1
 // Function 1
 const multiply = (a, b) => a * b;
+console.log(multiply(1,2));
 
 // Function 2
 const isPositive = (number) => number >= 0;
+console.log(isPositive(2));
 
 // Function 3
 const getRandomNumber = () => Math.random();
+console.log(getRandomNumber());
 
 // Function 4
 document.addEventListener("click", () => console.log("Clicked!"));
@@ -17,3 +20,27 @@ const createUser = (name ="Anonymous", age = 18, isAdmin =false) => ({
     age,
     isAdmin,
 });
+console.log(createUser());
+
+//ex3
+const mergeArrays = (...arrs) => [].concat(...arrs);
+console.log(mergeArrays([1,2],[3,4]));
+const sumAll = (...numbers) => numbers.reduce((total,num)=>total + num,0);
+console.log(sumAll(3,7,8,12));
+const createProduct = (name = "ip",price = 10000,description = "abcc") => ({
+  name,
+  price,
+  description,
+});
+console.log(createProduct());
+
+//ex4
+const shoppingCart = (customerName, ...products)=>{
+    return {
+        customer: customerName || "Khách",
+        items: products.length ? products : ["không có sp nào"],
+        itemCount: products.length,
+        message: `Khách hàng ${customerName} đã mua ${products.length} sản phẩm`,
+    };
+};
+console.log(shoppingCart("Hien","sp1","sp2"));
