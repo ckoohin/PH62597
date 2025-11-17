d15c55c5bf9016cac7dcc59fcab5275725ef4d71
ckoohin <chuhien.nhat@gmail.com>
2025-11-17 15:55:52 +0700

    navlink

--- lab5/react-app/src/components/common/Navigation.jsx
+++ lab5/react-app/src/components/common/Navigation.jsx
@@ -3,17 +3,17 @@ import { Link, NavLink } from "react-router-dom";
 function Navigation() {
     return (
         <nav className="my-2">
-        <ul className="flex justify-around text-xl font-bold">
-            <li>
+        <ul className="flex justify-around text-xl font-bol">
+            <li className="hover:text-blue-600">
             <Link to="/">Home</Link>
             </li>
-            <li>
+            <li className="hover:text-blue-600">
             <NavLink to="/about">About</NavLink> 
             </li>
-            <li>
+            <li className="hover:text-blue-600">
             <a href="/">Booking</a>
             </li>
-            <li>
+            <li className="hover:text-blue-600">
             <a href="/">Contact</a>
             </li>
         </ul>
--- lab5/react-app/src/pages/About.jsx
+++ lab5/react-app/src/pages/About.jsx
@@ -1,10 +1,15 @@
+import Navigation from "../components/common/Navigation";
+
 function About() {
     return (
-        <div className="max-w-4xl mx-auto p-6">
-            <h1 className="text-3xl font-bold text-gray-800 mb-4">Giới thiệu</h1>
-            <p className="text-gray-600">
-                Về chúng tôi
-            </p>
+        <div>
+            <Navigation/>
+            <div className="max-w-4xl mx-auto p-6">
+                <h1 className="text-3xl font-bold text-gray-800 mb-4">Giới thiệu</h1>
+                <p className="text-gray-600">
+                    Về chúng tôi
+                </p>
+            </div>
         </div>
     );
 }
